b7fb05dafef3e7aa60f745102f15392c06587a2c
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2021-12-14 20:57:22 -0600

    finished

---
+++ src/people.js
@@ -0,0 +1,13 @@
+export let people = [{
+    name: "john",
+    age: 30,
+    hairColor: "brown",
+}, {
+    name: "jane",
+    age: 25,
+    hairColor: "blonde",
+}, {
+    name: "jack",
+    age: 20,
+    hairColor: "black",
+}];
--- src/server.js
+++ src/server.js
@@ -1,6 +1,45 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { promises as fs } from 'fs';
+import {people} from './people';
+
+
 
 let app = express();
+
+app.use(bodyParser.json());
+
+app.get('/hello', (req, res) => {
+    res.send("Hey!");
+});
+
+app.get('/people', (req, res) => {
+    res.json(people);
+});
+
+app.get('/people/:name', (req, res) =>{
+    let { name } = req.params;
+
+    let person = people.find(x => x.name === name);
+
+    res.json(person);
+});
+
+app.get('/file-data', async (req, res) => {
+    let data = await fs.readFile(__dirname + '/people-data.json');
+    let people = JSON.parse(data);
+
+    res.json(people);
+});
+
+
+app.post('/people', (req, res) => {
+    let newPerson = req.body;
+    people.push(newPerson);
+    res.json(people);
+
+});
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
